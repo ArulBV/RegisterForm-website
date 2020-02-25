0713f887513bfe60271d5407c7e389f5930e67f2
B ARUL GNANA JOTHI <47982416+ArulBV@users.noreply.github.com>
2020-02-25 21:32:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function gtf()
+{
+	document.getElementById("cont").style.display="none";
+	document.getElementById("frm").style.display="block";
+}
+function gtc()
+{
+	document.getElementById("cont").style.display="block";
+	document.getElementById("frm").style.display="none";
+}
